216ddefa4184feda9f84018050ff66009891fa1e
Alireza <idraidpny@gmail.com>
2025-09-03 01:05:23 +0330

    mpu implemented DONE

--- api/sensors.py
+++ api/sensors.py
@@ -1,8 +1,6 @@
 class SensorFormatter:
     """reformats the data that has been read from arduino and rp for each relative sensor"""
 
-
-
     def __init__(self, ardu_data):
         self.roll = ardu_data[0]
         self.pitch = ardu_data[1]
@@ -40,20 +38,20 @@ class MPU:
 
     # (-------PITCH------)
 
-    def pitch_pos(self):
+    def get_pitch_power(self):
         pitch = abs(self.pitch)
+        t_power = 40  # change this for further power management
         while pitch >= 10:
             pitch //= 10
-        truster_power = pitch * 40
-        # m2 up / m5 down
-        return f"m2={1500 - truster_power} m5={(-1 * truster_power) + 1500}"
-
-    def pitch_neg(self):
-        pitch = abs(self.pitch)
-        while pitch >= 10:
-            pitch //= 10
-        truster_power = pitch * 40
-        # m2 down / m5 up
-        return f"m2={truster_power + 1500} m5={truster_power + 1500}"
+        truster_power = pitch * t_power
+        return truster_power
 
     # (-------YAW---------)
+    def get_yaw_power(self , diff):
+        diff = abs(diff)
+        t_power = 20  # change this for further power management
+        while diff >= 10:
+            diff //= 10
+        truster_power = diff * t_power
+        return truster_power
+
--- api/stabilize.py
+++ api/stabilize.py
@@ -1,26 +1,54 @@
-from . import sensors
-
-
 # TODO : write an algorithm to stabilize the robot
 # ToDo : write a code to stable the robot in saved yaw
 
 class Stabilize:
 
-    def __init__(self, mpu):
-
+    def __init__(self, mpu, saved_yaw):
         self.mpu = mpu
+        self.saved_yaw = saved_yaw
+        self.pitch_error = 10
+        self.yaw_error = 8
 
     def make_stable(self):
+        return self.get_tilt_direction() + self.get_turn_direction()
 
-        return self.pitched()
+    def get_tilt_direction(self):
 
-    def pitched(self):
+        if self.mpu.pitch > self.pitch_error:
 
-        if self.mpu.pitch > 10:
-            print("WARNING : Stabilizing the robot for --PITCH-- ")
-            return self.mpu.pitch_pos()
-        elif self.mpu.pitch < -10:
-            print("WARNING : Stabilizing the robot for --PITCH-- ")
-            return self.mpu.pitch_neg()
+            print("WARNING : Tilting the Penguin ")
+            truster_power = self.mpu.get_pitch_power()
+            return f"m2={1500 - truster_power} m5={(-1 * truster_power) + 1500}"
 
-        return "m2=1500 m5=1500"
+        elif self.mpu.pitch < -self.pitch_error:
+
+            print("WARNING : Tilting the Penguin ")
+            truster_power = self.mpu.get_pitch_power()
+            return f"m2={truster_power + 1500} m5={truster_power + 1500}"
+
+        return ""
+
+    def get_turn_direction(self):
+        """
+        Compare current yaw with saved yaw and return whether to turn left or right
+        to realign with saved_yaw. Yaw values are circular in [0, 360].
+        """
+        if self.saved_yaw is None:
+            return ""
+        # Normalize values to [0, 360)
+        current = self.mpu.yaw % 360
+        target = int(self.saved_yaw) % 360
+
+        # Calculate smallest angular difference (-180 to 180)
+        diff = (target - current + 540) % 360 - 180
+        # if robot turned wrongly should change the function calls
+
+        if diff > self.yaw_error:
+            truster_power = self.mpu.get_yaw_power(diff)
+            print("WARNING : Turning the Penguin to left")
+            return f"m1={(-1 * truster_power) + 1500}"  # Turning left reduces yaw difference
+        elif diff < -self.yaw_error:
+            truster_power = self.mpu.get_yaw_power(diff)
+            print("WARNING : Turning the Penguin to right")
+            return f"m1={(-1 * truster_power) + 1500}"
+        return ""
